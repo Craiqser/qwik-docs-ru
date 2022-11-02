44dfdc785ec522d626f955175582d0d4adf11e9b
Sobolev Sergey <craiqser@gmail.com>
2022-11-02 11:51:28 +0300

    docs(tutorial): qrl data translate

--- src/routes/tutorial/qrl/data/problem/app.tsx
+++ src/routes/tutorial/qrl/data/problem/app.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export const App = component$(() => {
   return (
     <>
-      <button onClick$={async () => alert('Hello World!')}>click me</button>
+      <button onClick$={async () => alert('Привет, мир!')}>нажми меня</button>
     </>
   );
 });
--- src/routes/tutorial/qrl/data/solution/app.tsx
+++ src/routes/tutorial/qrl/data/solution/app.tsx
@@ -3,7 +3,7 @@ import { component$, $ } from '@builder.io/qwik';
 export const App = component$(() => {
   return (
     <>
-      <button onClick$={async () => alert(await $('Hello World!').resolve())}>click me</button>
+      <button onClick$={async () => alert(await $('Привет, мир!').resolve())}>нажми меня</button>
     </>
   );
 });
