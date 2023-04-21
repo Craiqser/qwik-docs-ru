790881cf5b261d4c6f978658411287e4f53150a1
Sobolev Sergey <craiqser@gmail.com>
2023-04-21 13:35:30 +0300

    chore: bump to #3864

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Translate status
 
-- Last update - 20.04.2023;
+- Last update - 21.04.2023;
 - Translated: Qwik, QwikCity, Examples, Tutorials.
 
 <br>
---
+++ src/components/contributors/index.tsx
@@ -0,0 +1,29 @@
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import styles from './contributors.css?inline';
+import { useDocumentHead } from '@builder.io/qwik-city';
+
+export default component$(() => {
+  useStylesScoped$(styles);
+  const { frontmatter } = useDocumentHead();
+  const contributors = frontmatter.contributors || [];
+
+  if (!contributors.length) {
+    return null;
+  }
+
+  return (
+    <div class="wrapper card">
+      <h3>Участники</h3>
+      <p>Спасибо всем участникам, которые помогли сделать эту документацию лучше!</p>
+      <ul class="list">
+        {contributors.map((contributor: string) => (
+          <li key={`contributor-${contributor}`} class="contributor">
+            <a href={`https://github.com/${contributor}`} target="_blank" rel="noreferrer">
+              <img src={`https://github.com/${contributor}.png`} alt={contributor} class="avatar" />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+});
