984adb087d1992c445694d83c1d4c6196a7d0d88
Sobolev Sergey <craiqser@gmail.com>
2023-05-06 15:39:23 +0300

    chore: bump to #4035

--- src/routes/tutorial/store/recursive/solution/app.tsx
+++ src/routes/tutorial/store/recursive/solution/app.tsx
@@ -1,19 +1,25 @@
 import { component$, useStore } from '@builder.io/qwik';
 
 export default component$(() => {
-  const store = useStore({ counter: { count: 0 } }, { deep: true });
+  const store = useStore({ counter: { count: 0 }, list: [0] }, { deep: false });
 
   return (
     <>
-      <Display counter={store.counter} />
-      <button onClick$={() => store.counter.count++}>+1</button>
+      <Display counter={store.counter} list={store.list} />
+      <button onClick$={() => (store.counter = { count: ++store.counter.count })}>+1 к счтёчику</button>
+      <button onClick$={() => (store.list = [...store.list, 0])}>+1 к списку элементов</button>
     </>
   );
 });
 
 interface DisplayProps {
   counter: { count: number };
+  list: number[];
 }
 export const Display = component$((props: DisplayProps) => {
-  return <p>Счётчик: {props.counter.count}</p>;
+  return (
+    <p>
+      Счётчик: {props.counter.count}, Длина списка: {props.list.length}
+    </p>
+  );
 });
